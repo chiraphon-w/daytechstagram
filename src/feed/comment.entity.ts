d1644b09c1adbe6a686923cc403d49c206cc345f
chiraphon-w <chw242542@gmail.com>
2021-05-10 13:11:24 +0700

    edit table post, comment

--- src/feed/comment.entity.ts
+++ src/feed/comment.entity.ts
@@ -9,13 +9,9 @@ import {
 import * as bcrypt from 'bcrypt';
 
 @Entity()
-@Unique(['username'])
 export class Comment extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
-
-  @Column()
-  username: string;
 
   @Column()
   desc: string;
--- src/feed/post.entity.ts
+++ src/feed/post.entity.ts
@@ -1,29 +1,24 @@
 import {
-    BaseEntity,
-    Column,
-    Entity,
-    PrimaryGeneratedColumn,
-    Unique,
-    UpdateDateColumn,
-  } from 'typeorm';
-  import * as bcrypt from 'bcrypt';
-  
-  @Entity()
-  @Unique(['username'])
-  export class Post extends BaseEntity {
-    @PrimaryGeneratedColumn()
-    id: number;
-  
-    @Column()
-    username: string;
-  
-    @Column()
-    image: string;
-  
-    @Column()
-    desc: string;
-  
-    @UpdateDateColumn()
-    updated: Date;
-  }
-  
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
+@Entity()
+export class Post extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  image: string;
+
+  @Column()
+  desc: string;
+
+  @UpdateDateColumn()
+  updated: Date;
+}
